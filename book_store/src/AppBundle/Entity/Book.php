d46e3396bd7585bf20c31bf7950c5ff21ebff7ab
Reboul Anthony <anthony12041989@gmail.com>
2019-03-13 16:00:32 +0100

    mise en place du CRUD pour les livres

--- book_store/src/AppBundle/Entity/Book.php
+++ book_store/src/AppBundle/Entity/Book.php
@@ -10,7 +10,7 @@ use Doctrine\ORM\Mapping as ORM;
  * @ORM\Table(name="books")
  * @ORM\Entity(repositoryClass="AppBundle\Repository\BooksRepository")
  */
-class Books
+class Book
 {
     /**
      * @var int
